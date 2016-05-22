9251841cf9506ba2fe033b38c2feb4dea4a50949
Benjamin Mildren <Ben@Benjamins-MBP.home>
2016-05-22 23:24:23 +0100

    Updated info popup

--- views/Components/Info.jsx
+++ views/Components/Info.jsx
@@ -7,6 +7,7 @@ const Info = React.createClass({
         <input type="checkbox" className="Info_Toggle" />
         <div className="Info_Popup">
           This is an open source Pokedéx made for the community.
+          The data is for X & Y and ORAS.
           It is meant to be concise, minimal and fast.
           If you want to help the project please talk to me on Twitter or Github.
           <a href="http://www.google.com">Github</a>
