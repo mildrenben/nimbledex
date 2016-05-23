9e9832cb1773f459ceca9aa19acb263f8973e854
Benjamin Mildren <Ben@Benjamins-MBP.home>
2016-05-24 00:57:42 +0100

    404 page design

--- views/404.jsx
+++ views/404.jsx
@@ -1,11 +1,29 @@
 const React = require('react');
+const Nav = require('./Nav/Nav');
 
 const page404 = React.createClass({
   render() {
     return (
-      <div className="404">
-        404 bitches
-      </div>
+      <html>
+        <head>
+          <link rel="stylesheet" type="text/css" href="/css/404.css" />
+          <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css' />
+          <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css' />
+        </head>
+        <body className="page404">
+          <Nav />
+          <main>
+            <div className="MissingnoWrap">
+              <img src="/img/sprites/missingno.png" />
+              <h5>Missingno</h5>
+            </div>
+            <p>We could not find the Pokémon you were looking for</p>
+          </main>
+          <script src="/js/script.js"> </script>
+          <script src="/js/awesomplete.min.js"> </script>
+          <script src="/js/typeahead.js"> </script>
+        </body>
+      </html>
     )
   },
 });
