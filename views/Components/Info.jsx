373c2d19492cd83ad0ab4737020d971c09a89a51
Benjamin Mildren <Ben@Benjamins-MBP.home>
2016-06-07 18:36:37 +0100

    Fixed typo

--- views/Components/Info.jsx
+++ views/Components/Info.jsx
@@ -7,7 +7,7 @@ const Info = React.createClass({
         <input type="checkbox" className="Info_Toggle" />
         <div className="Info_Popup">
           <p>This is an open source Pokedéx made for the community.</p>
-          <p>The data is for ORAS and I will try to always keep the data conistent with the latest game.</p>
+          <p>The data is for ORAS and I will try to always keep the data consistent with the latest game.</p>
           <p>It is meant to be concise, minimal and fast.</p>
           If you want to help the project please talk to me on Twitter or Github.
           <a href="https://github.com/mildrenben/nimbledex">Github</a>
