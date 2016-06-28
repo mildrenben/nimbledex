f3f20e638b9833c9553ae9fa12e5ac471797fbfa
Benjamin Mildren <Ben@Benjamins-MBP.home>
2016-06-28 20:54:59 +0100

    Removed loop for nginx

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,3 @@ var infoPopup = document.getElementsByClassName('Info_Popup')[0];
 infoToggle.addEventListener('click', function() {
   infoPopup.classList.toggle('visible');
 });
-
-setTimeout(() => {
-  window.location.href = parseInt(window.location.href.slice(window.location.href.indexOf('com') + 4, window.location.href.length)) + 1;
-}, 4000);
