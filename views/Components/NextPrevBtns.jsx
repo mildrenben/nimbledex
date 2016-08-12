ea94185f61b6dc2b6ae97c8530e69740f37f1a65
Benjamin Mildren <Ben@Benjamins-MBP.home>
2016-08-13 00:17:01 +0100

    Fixed bug with next prev on first and last mon

--- views/Components/NextPrevBtns.jsx
+++ views/Components/NextPrevBtns.jsx
@@ -4,12 +4,14 @@ const NextPrevBtns = React.createClass({
   render() {
     return (
       <div className="NextPrevBtns">
+        { this.props.prev !== 0 &&
         <a href={this.props.prev} className="NextPrevBtns_Btn NextPrevBtns_Btn--prev">
           Prev
-        </a>
+        </a> }
+        { this.props.next !== 722 &&
         <a href={this.props.next} className="NextPrevBtns_Btn NextPrevBtns_Btn--next">
           Next
-        </a>
+        </a> }
       </div>
     );
   },
